e8a9e4a90c426aab84708e33c4a414e45931657c
tarikkeskin <tarik.keskin007@gmail.com>
2022-04-21 13:28:45 +0300

    Bottom_navigationbar_setup_done

--- app/src/main/java/com/example/getirdesign/MainActivity.java
+++ app/src/main/java/com/example/getirdesign/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.getirdesign;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.getirdesign.databinding.BottomNavigationBinding;
 
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationBinding tasarim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        tasarim = BottomNavigationBinding.inflate(getLayoutInflater());
+        setContentView(tasarim.getRoot());
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navHostFragment);
+        NavigationUI.setupWithNavController(tasarim.bottomNav,navHostFragment.getNavController());
+
     }
 }
---
+++ app/src/main/java/com/example/getirdesign/fragments/GiftsFragment.java
@@ -0,0 +1,23 @@
+package com.example.getirdesign.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.getirdesign.R;
+
+
+public class GiftsFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gifts, container, false);
+    }
+}
