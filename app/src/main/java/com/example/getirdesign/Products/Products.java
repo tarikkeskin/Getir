9d379eaf7b135c4e99fbaec6178a9a9b88c4d258
tarikkeskin <tarik.keskin007@gmail.com>
2022-04-21 16:01:16 +0300

    products_carddesign_adapter

---
+++ app/src/main/java/com/example/getirdesign/Products/Products.java
@@ -0,0 +1,63 @@
+package com.example.getirdesign.Products;
+
+import java.io.Serializable;
+
+public class Products implements Serializable {
+
+    private int productId;
+    private String productName;
+    private String productImage;
+    private double productPrice;
+    private int productWeight;
+
+    public Products() {
+    }
+
+    public Products(int productId, String productName, String productImage, double productPrice, int productWeight) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productImage = productImage;
+        this.productPrice = productPrice;
+        this.productWeight = productWeight;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductImage() {
+        return productImage;
+    }
+
+    public void setProductImage(String productImage) {
+        this.productImage = productImage;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public int getProductWeight() {
+        return productWeight;
+    }
+
+    public void setProductWeight(int productWeight) {
+        this.productWeight = productWeight;
+    }
+}
---
+++ app/src/main/java/com/example/getirdesign/adapters/ProductsAdapter.java
@@ -0,0 +1,60 @@
+package com.example.getirdesign.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.getirdesign.Products.Products;
+import com.example.getirdesign.databinding.CardProductDesingBinding;
+
+import java.util.List;
+
+public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.CardDesignAttachment> {
+    private Context mContext;
+    private List<Products> productsList;
+
+    public ProductsAdapter(Context mContext, List<Products> productsList) {
+        this.mContext = mContext;
+        this.productsList = productsList;
+    }
+
+    public class CardDesignAttachment extends RecyclerView.ViewHolder{
+        private CardProductDesingBinding tasarim;
+
+        public CardDesignAttachment(CardProductDesingBinding tasarim) {
+            super(tasarim.getRoot());
+            this.tasarim = tasarim;
+        }
+    }
+
+    @NonNull
+    @Override
+    public CardDesignAttachment onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+        CardProductDesingBinding tasarim = CardProductDesingBinding.inflate(layoutInflater,parent,false);
+        return new CardDesignAttachment(tasarim);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CardDesignAttachment holder, int position) {
+        Products product = productsList.get(position);
+        CardProductDesingBinding t = holder.tasarim;
+
+        t.imageViewProduct.setImageResource(
+                mContext.getResources().getIdentifier(product.getProductImage(),"drawable",mContext.getPackageName()));
+        t.textViewProductName.setText(product.getProductName());
+        t.textViewProductPrice.setText(product.getProductPrice()+" $");
+        t.textViewWeight.setText(product.getProductWeight());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return productsList.size();
+    }
+
+}
